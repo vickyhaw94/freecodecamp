ba90d3cb54effdca7f6e6c031f5462e9f60b9603
vickyhaw94 <weikee070194@gmail.com>
2016-11-19 21:51:21 +0800

    Building JavaScript Objects

---
+++ Basic_Javascript/building_javascript_object.js
@@ -0,0 +1,23 @@
+//Instructions
+//Make an object that represents a dog called myDog which contains the properties "name" (a string), 
+//"legs", "tails" and "friends".
+
+//You can set these object properties to whatever values you want, as long "name" is a string, 
+//"legs" and "tails" are numbers, and "friends" is an array.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "GZ",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["GY"]
+};
