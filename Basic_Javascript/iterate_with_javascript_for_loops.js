466fdf015a5bbbf6d5f036e452a3301629b97b13
vickyhaw94 <weikee070194@gmail.com>
2016-11-20 19:26:26 +0800

    iterate with javascript for loops

---
+++ Basic_Javascript/iterate_with_javascript_for_loops.js
@@ -0,0 +1,19 @@
+//Instructions
+//Use a for loop to work to push the values 1 through 5 onto myArray.
+
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for(var i = 1; i < 6; i++){
+  myArray.push(i);
+}
+
