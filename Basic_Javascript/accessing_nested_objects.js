18ceb2b452211509603d3fd16ac230e799d719a8
vickyhaw94 <weikee070194@gmail.com>
2016-11-20 10:48:02 +0800

    accessing nested objects

---
+++ Basic_Javascript/accessing_nested_objects.js
@@ -0,0 +1,41 @@
+/*The sub-properties of objects can be accessed by chaining together the dot or bracket notation.
+
+Here is a nested object:
+
+var ourStorage = {
+  "desk": {
+    "drawer": "stapler"
+  },
+  "cabinet": {
+    "top drawer": { 
+      "folder1": "a file",
+      "folder2": "secrets"
+    },
+    "bottom drawer": "soda"
+  }
+};
+ourStorage.cabinet["top drawer"].folder2;  // "secrets"
+ourStorage.desk.drawer; // "stapler"
+Instructions
+Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable.
+Use bracket notation for properties with a space in their name.
+*/
+
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+// Only change code below this line
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
+
