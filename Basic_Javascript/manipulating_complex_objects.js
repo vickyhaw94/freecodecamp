7b4f9477dc473e941f476e598c87427ff408a33a
vickyhaw94 <weikee070194@gmail.com>
2016-11-20 10:39:09 +0800

    manipulating complex objects

---
+++ Basic_Javascript/manipulating_complex_objects.js
@@ -0,0 +1,45 @@
+/*JavaScript Object Notation or JSON is a related data interchange format used to store data.
+
+{
+  "artist": "Daft Punk",
+  "title": "Homework",
+  "release_year": 1997,
+  "formats": [ 
+    "CD",
+    "Cassette",
+    "LP"
+  ],
+  "gold": true
+}
+Note
+You will need to place a comma after every object in the array, unless it is the last object in the array.
+
+Instructions
+Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+*/
+
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  {
+    "artist" : "JB",
+    "title"  : "BF",
+    "release_year" : 2015,
+    "formats" : [
+      "SC",
+      "T8",
+      "PL"
+    ]
+  }
+  // Add record here
+];
+
