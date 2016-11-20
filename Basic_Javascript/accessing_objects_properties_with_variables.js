562b92ee4fe91edf64d7828803034d1a95c31bba
vickyhaw94 <weikee070194@gmail.com>
2016-11-20 08:50:46 +0800

    accessing objects properties with objects

---
+++ Basic_Javascript/accessing_objects_properties_with_variables.js
@@ -0,0 +1,15 @@
+// Instructions
+//Use the playerNumber variable to lookup player 16 in testObj using bracket notation.
+
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
