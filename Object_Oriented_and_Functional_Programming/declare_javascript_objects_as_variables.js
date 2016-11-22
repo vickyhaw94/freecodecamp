de33984916deb5a476316d9e948a57a0f8dd72ea
vickyhaw94 <weikee070194@gmail.com>
2016-11-22 22:13:22 +0800

    declare javascript objects as variables

---
+++ Object_Oriented_and_Functional_Programming/declare_javascript_objects_as_variables.js
@@ -0,0 +1,18 @@
+/*Before we dive into Object Oriented Programming, let's revisit JavaScript objects.
+
+Give your motorBike object a wheels, engines and seats attribute and set them to numbers.*/
+
+
+var car = {
+  "wheels":4,
+  "engines":1,
+  "seats":5
+};
+
+var motorBike = {
+  "wheels":2,
+  "engines":1,
+  "seats":2
+  // Only change code below this line.
+
+};
