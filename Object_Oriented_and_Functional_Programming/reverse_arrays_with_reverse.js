4fb9f03906a74a7f357c919f1c9796bfcbb78f79
vickyhaw94 <weikee070194@gmail.com>
2016-11-23 20:26:35 +0800

    reverse arrays with reverse

---
+++ Object_Oriented_and_Functional_Programming/reverse_arrays_with_reverse.js
@@ -0,0 +1,18 @@
+/*You can use the reverse method to reverse the elements of an array.
+
+reverse is another array method that alters the array in place, but it 
+also returns the reversed array.
+
+var myArray = [1, 2, 3];
+myArray.reverse();
+returns [3, 2, 1]
+
+Use reverse to reverse the array variable and assign it to newArray.*/
+
+
+var array = [1,2,3,4,5,6,7];
+var newArray = [];
+
+// Only change code below this line.
+
+newArray = array.reverse();
